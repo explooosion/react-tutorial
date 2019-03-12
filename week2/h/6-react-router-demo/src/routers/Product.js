d8d287fdeae2f775d126fd4720fec9e9f130c854
Robby <ta7382@gmail.com>
2019-03-12 23:03:00 +0800

    Add router example

---
+++ week2/h/6-react-router-demo/src/routers/Product.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Product extends Component {
+  render() {
+    return (
+      <div className="product">
+        <h1>Product</h1>
+      </div>
+    );
+  }
+}
+
+export default Product;
