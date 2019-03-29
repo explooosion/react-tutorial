105d52f51d2b07d6bce052d171d1100fdc2ddc2e
Robby <ta7382@gmail.com>
2019-03-29 15:48:06 +0800

    Add wee4 e-3

---
+++ week4/e/3-add-state-data/src/components/Footer.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Footer extends Component {
+  render() {
+    return (
+      <div>
+        <footer>
+          <p>COPYRIGHT © 2019 NUTC_JACK_LAB ALL RIGHTS RESERVED</p>
+        </footer>
+      </div>
+    )
+  }
+}
+
+export default Footer;
